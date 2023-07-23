7a20065f57d7fe44b9a1f7ce84b9dd791fd5d64b
Xeronax <32139713+Xeronax@users.noreply.github.com>
2023-07-23 09:13:56 -0500

    DFC Display Bug fix

--- src/logic/card/mergeDfc.js
+++ src/logic/card/mergeDfc.js
@@ -5,7 +5,8 @@ const mergeImages = require('merge-images');
 const axios = require('axios');
 const fs = require('fs');
 const { Canvas, Image } = require('canvas');
-const { getImage } = require('../../server')
+const { getImage } = require('../../server');
+const sharp = require('sharp');
 
 
 module.exports = {
@@ -23,55 +24,21 @@ module.exports = {
 
 }
 
-async function downloadImage(url, cardName) {
-
-    const filePath = `./src/imageDump/${cardName.replace(/ /g, '_')}.png`;
-
-    const response = await axios({
-
-        method: 'GET',
-        url: url,
-        responseType: 'stream'
-
-    })
-        
-    if(response.status != 200) return null;
-
-    return new Promise((resolve, reject) => {
-
-        response.data.pipe(fs.createWriteStream(filePath))
-        .on('finish', () => {
-
-            console.log("Image downloaded successfully.");
-
-            resolve(filePath);
-
-        })
-        .on('error', err => {
-
-            console.error(`${err.message}\n${err.stack}`);
-
-            reject(err);
-
-        })
-
-    })
-
-}
-
 async function mergeFaces( frontPath, backPath ) {
+
+    const dimensions = await getDimensions;
 
     return mergeImages([ 
 
         { src: frontPath, x: 0, y: 0 },
-        { src: backPath, x: 488, y: 0 }
+        { src: backPath, x: dimensions.front.length, y: 0 }
 
     ], {
 
         Canvas: Canvas,
         Image: Image,
-        width: 976,
-        height: 680
+        width: (dimensions.front.length + dimensions.back.length),
+        height: (Math.max(dimensions.front.height, dimensions.back.height))
 
     })
     .then(b64 => {
@@ -122,4 +89,64 @@ async function mergeFaces( frontPath, backPath ) {
         return null;
 
     })
+}
+
+async function downloadImage(url, cardName) {
+
+    const filePath = `./src/imageDump/${cardName.replace(/ /g, '_')}.png`;
+
+    const response = await axios({
+
+        method: 'GET',
+        url: url,
+        responseType: 'stream'
+
+    })
+        
+    if(response.status != 200) return null;
+
+    return new Promise((resolve, reject) => {
+
+        response.data.pipe(fs.createWriteStream(filePath))
+        .on('finish', () => {
+
+            console.log("Image downloaded successfully.");
+
+            resolve(filePath);
+
+        })
+        .on('error', err => {
+
+            console.error(`${err.message}\n${err.stack}`);
+
+            reject(err);
+
+        })
+
+    })
+
+}
+
+async function getDimensions(frontImagePath, backImagePath) {
+
+    const frontMetaData = sharp(frontImagePath).metadata;
+    const backMetaData = sharp(backImagePath).metadata;
+    const front = {
+
+        length: frontMetaData.length,
+        height:  frontMetaData.height
+
+    }
+    console.log(front);
+
+    const back = {
+
+        length: backMetaData.length,
+        height: backMetaData.height,
+
+    }
+    console.log(back);
+    
+    return { front, back };
+    
 }
